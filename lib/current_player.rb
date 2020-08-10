038b57ed462a10b6e7fdcd181930530552565062
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-10 17:30:15 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
     board.each do |move|
       counter = 0
       if "#{move}" == "X" || "#{move}" == "O"
-      counter += 1
       else puts "Game over!"
+      counter += 1
     end
 end
